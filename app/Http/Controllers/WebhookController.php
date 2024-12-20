68cc6a1e1695b960e35d3794871db4e7c99e112b
GRIB-MIREA <litdim@bk.ru>
2024-12-21 01:02:27 +0300

    dev

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,37 +9,20 @@ class WebhookController extends Controller
 {
     public function handleWebhook(Request $request)
     {
-        $update = $request->all();
-
-        // Проверяем, есть ли сообщение и команда
-        if (isset($update['message'])) {
-            $chatId = $update['message']['chat']['id'];
-            $text = $update['message']['text'];
-
-            // Проверяем, является ли сообщение командой /start
-            if ($text === '/start') {
-                $responseText = "Добро пожаловать! Я ваш бот, как я могу помочь?";
-                $this->sendMessage($chatId, $responseText);
-            } else {
-                // Обрабатываем другие команды или текстовые сообщения
-                $this->sendMessage($chatId, "Вы написали: " . $text);
-            }
-        }
-
-        return response()->json(['status' => 'success']);
+        return response()->json(['status' => 'Webhook received']);
     }
 
-    private function sendMessage($chatId, $text)
-    {
-        $token = env('TELEGRAM_BOT_TOKEN'); // Получите токен из .env
-        $url = "https://api.telegram.org/bot{$token}/sendMessage";
+    // private function sendMessage($chatId, $text)
+    // {
+    //     $token = env('TELEGRAM_BOT_TOKEN'); // Получите токен из .env
+    //     $url = "https://api.telegram.org/bot{$token}/sendMessage";
 
-        $data = [
-            'chat_id' => $chatId,
-            'text' => $text,
-        ];
+    //     $data = [
+    //         'chat_id' => $chatId,
+    //         'text' => $text,
+    //     ];
 
-        // Отправка POST-запроса
-        Http::post($url, $data);
-    }
+    //     // Отправка POST-запроса
+    //     Http::post($url, $data);
+    // }
 }
