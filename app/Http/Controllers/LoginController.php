a917b818c11fff7bbaa4239a24e8e338f25afbf5
GRIB-MIREA <litdim@bk.ru>
2024-12-21 05:12:22 +0300

    dev

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,15 +6,24 @@ use App\Models\Player;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
     public function auth(Request $request){
         $url = $request->input('photo_url');
-        $ext = explode('.', $url);
-        $ext = $ext[count($ext) - 1];
+        $ext = pathinfo($url, PATHINFO_EXTENSION);
         $file_name = uniqid().'.'.$ext;
-        Storage::putFileAs('public/assets/img', $request->input('photo_url'), $file_name);
+        $response = Http::get($url);
+
+        if ($response->successful()) {
+            // Сохраняем изображение в хранилище
+            Storage::put('public/assets/img/' . $file_name, $response->body());
+        } else {
+            // Обработка ошибки, если изображение не удалось загрузить
+            return response()->json(['error' => 'Не удалось загрузить изображение'], 500);
+        }
+        //Storage::putFileAs('public/assets/img', $request->input('photo_url'), $file_name);
 
         $player = Player::updateOrCreate(['telegram_id' => $request->input('id')], [
             'telegram_id' => $request->input('id'),
