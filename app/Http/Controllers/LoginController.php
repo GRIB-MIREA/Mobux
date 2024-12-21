e3d1b499d41e6740865f7ff4aab3430e6d40ec4a
GRIB-MIREA <litdim@bk.ru>
2024-12-21 05:44:50 +0300

    dev

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Http;
 
@@ -30,10 +31,12 @@ class LoginController extends Controller
             'last_name' => $request->input('last_name'),
             'telegram_username' => $request->input('username'),
             'image' => $request->input('photo_url'),
+            'password' => Hash::make(123),
         ]);
-        if(Auth::attempt(['telegram_id' => $user->telegram_id])){
+        if(Auth::attempt(['telegram_id' => $user->telegram_id, 'password' => 123])){
             $request->session()->regenerate();
             return response()->redirectTo('/bot');
-        };
+        }
+        return response()->redirectTo('/');
     }
 }
