13c8473d8a2838b3050ebf609bbd823845a54ba5
GRIB-MIREA <litdim@bk.ru>
2024-12-21 04:56:53 +0300

    dev

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class LoginController extends Controller
 {
     public function auth(Request $request){
-        dd($request->all());
+        $url = $request->input('photo_url');
+        $ext = explode('.', $url);
+        $ext = $ext[count($ext) - 1];
+        $file_name = uniqid().'.'.$ext;
+        Storage::putFileAs('public/assets/img', $request->input('photo_url'), $file_name);
+
+        $player = Player::updateOrCreate(['telegram_id' => $request->input('id')], [
+            'telegram_id' => $request->input('id'),
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'telegram_username' => $request->input('username'),
+            'image' => $file_name,
+        ]);
+        if(Auth::attempt(['telegram_id' => $player->telegram_id])){
+            $request->session()->regenerate();
+            return response()->redirectTo('/bot');
+        };
     }
 }
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     use HasFactory;
+
+    protected $table = 'players';
+    protected $fillable = [
+        'telegram_id',
+        'first_name',
+        'last_name',
+        'telegram_username',
+        'image'
+    ];
 }
