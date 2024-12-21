1ccca1ec7f4858e116d50bb613cc373df3e3d92e
GRIB-MIREA <litdim@bk.ru>
2024-12-21 04:24:15 +0300

    dev

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <main class="container mx-auto mt-10">
-    <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="mobux_bot" data-size="large" data-auth-url="https://mobux.ru/api/auth" data-request-access="write"></script>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="right-0 p-6">
+        <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="mobux_bot" data-size="large" data-auth-url="https://mobux.ru/api/auth" data-request-access="write"></script>
+    </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div class="bg-white rounded-lg shadow-md p-4">
             <img src="https://via.placeholder.com/300" alt="Пример изображения" class="rounded-lg mb-4">
             <h2 class="text-xl font-semibold">Заголовок карточки</h2>
