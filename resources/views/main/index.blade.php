fae4e6e8540a530587afd461c8ee264e19ab1779
GRIB-MIREA <litdim@bk.ru>
2024-12-21 04:21:24 +0300

    dev

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <main class="container mx-auto mt-10">
+    <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="mobux_bot" data-size="large" data-auth-url="https://mobux.ru/api/auth" data-request-access="write"></script>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div class="bg-white rounded-lg shadow-md p-4">
             <img src="https://via.placeholder.com/300" alt="Пример изображения" class="rounded-lg mb-4">
