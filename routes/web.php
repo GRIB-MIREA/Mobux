3c3485b7ddd448f81669750b13ed951620744382
GRIB-MIREA <litdim@bk.ru>
2024-12-21 03:42:44 +0300

    dev

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\WebhookController;
 use App\Http\Controllers\TelegramController;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 /*
@@ -17,11 +18,15 @@ use Illuminate\Support\Facades\Http;
 |
 */
 
-$http = Http::post('https://api.telegram.org/bot7770123301:AAH_3y0slyu2-BSVEjJ2ruBFivIf5cbRLfQ/setWebhook', [
-    'url' => 'https://mobux.ru/api/webhook'
-])->json();
+// $http = Http::post('https://api.telegram.org/bot7770123301:AAH_3y0slyu2-BSVEjJ2ruBFivIf5cbRLfQ/setWebhook', [
+//     'url' => 'https://mobux.ru/api/webhook'
+// ])->json();
 
-dd($http);
+// dd($http);
+
+Route::get('/webhook-data', function() {
+    dd(Cache::get('webhook-data'));
+});
 
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('index');
