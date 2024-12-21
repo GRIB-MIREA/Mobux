8f443a01aaffd027c92e70fe3210face4147b1dd
GRIB-MIREA <litdim@bk.ru>
2024-12-21 03:58:14 +0300

    dev

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 
 class WebhookController extends Controller
 {
@@ -11,5 +12,21 @@ class WebhookController extends Controller
     {
         Cache::forever('webhook-data', $request->all());
         return true;
+    }
+
+    public function sendMessage(){
+        Http::post('https://api.telegram.org/bot7770123301:AAH_3y0slyu2-BSVEjJ2ruBFivIf5cbRLfQ/sendMessage', [
+            'chat_id' => 299814741,
+            'text' => 'Заходи в бота',
+            'reply_markup' => [
+                'inline_keyboard' => [
+                    [
+                        'web_app' => [
+                            'url' => 'https://t.me/mobux_bot/app'
+                        ]
+                    ]
+                ]
+            ]
+        ])->json();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
 
 Route::group(['namespace' => 'App\Http\Controllers\Bot', 'prefix' => 'bot'], function () {
     Route::get('/', 'IndexController')->name('bot.index');
+    Route::get('/', [WebhookController::class, 'sendMessage']);
 });
 
 Auth::routes();
