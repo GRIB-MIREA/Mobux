70b79908e4da08cee650f99317584402fb416192
GRIB-MIREA <litdim@bk.ru>
2024-12-21 04:28:26 +0300

    dev

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\WebhookController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('webhook', [WebhookController::class, 'index']);
+Route::any('auth', [LoginController::class, 'auth']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
