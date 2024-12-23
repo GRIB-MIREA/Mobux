176c86a76dfda2b8b4023f9e10a035dab53d2019
GRIB-MIREA <litdim@bk.ru>
2024-12-23 17:37:45 +0300

    webhook

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -5,6 +5,6 @@
         <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="mobux_bot" data-size="large" data-auth-url="https://mobux.ru/api/auth" data-request-access="write"></script>
     </div>
     <h1>MOBUX - Главная страница с описанием проекта</h1>
-    <a href="{{route('bot.index')}}">Перейти в бота</a>
+    <a class="text-white" href="{{route('bot.index')}}">Перейти в бота</a>
 </main>
 @endsection
