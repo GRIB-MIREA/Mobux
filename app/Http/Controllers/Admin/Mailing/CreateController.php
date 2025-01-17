8530b7bd7de4ae454929d4ed7a283a1f3e2e3958
GRIB-MIREA <litdim@bk.ru>
2025-01-17 03:02:56 +0300

    mailing

--- app/Http/Controllers/Admin/Mailing/CreateController.php
+++ app/Http/Controllers/Admin/Mailing/CreateController.php
@@ -26,7 +26,7 @@ class CreateController extends Controller
         $recipientsCount = $recipients->count();
 
         $message = $request->input('message');
-        $cleanedMessage = $this->convertToTelegramFormat($message);
+        $cleanedMessage = strip_tags($message);
 
         // Запускаем команду Artisan
         Artisan::call('telegram:send', ['message' => $cleanedMessage, 'parse_mode' => 'Markdown']);
@@ -38,21 +38,5 @@ class CreateController extends Controller
         ]);
 
         return redirect()->route('admin.mail.index')->with('success', 'Сообщение отправлено всем пользователям!');
-    }
-
-    private function convertToTelegramFormat($text) {
-        // Заменяем <strong> и <b> на * (жирный текст)
-        $text = preg_replace('/<(strong|b)>(.*?)<\/(strong|b)>/is', '*\$2*', $text);
-        // Заменяем <em> на _ (курсивный текст)
-        $text = preg_replace('/<em>(.*?)<\/em>/is', '_\$1_', $text);
-        // Заменяем <u> на __ (подчеркнутый текст)
-        $text = preg_replace('/<u>(.*?)<\/u>/is', '__\$1__', $text);
-        // Заменяем <br> на перенос строки
-        $text = preg_replace('/<br\s*\/?>/is', "\n", $text);
-        // Удаляем все остальные HTML-теги
-        $text = strip_tags($text);
-    
-        // Возвращаем очищенный и отформатированный текст
-        return $text;
     }
 }
